1c2155637a6bbe7230a7b2c00387e2684652d690
ssaitho <s.s.saito108@gmail.com>
2022-03-08 00:34:40 +0900

    add file

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import { useEffect, useRef, useState } from "react";
+import Link from 'next/link'
+import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -13,6 +16,42 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    // tocbot.init({
+    //   tocSelector: '.js-toc',
+    //   contentSelector: '.blog-detail-content',
+    //   headingSelector: 'h2',
+    //   hasInnerContainers: true,
+    //   scrollSmoothOffset: -100
+    // });
+    // var city = ["新宿", "渋谷", "池袋"];
+    let array = ref.current?.children
+    // let unko = Array.from( array ) ;
+    // let elm = [].slice.call(array)
+    var arr = [...array]
+    let filterItems = arr.filter(function(value) {
+      return value === "h2";
+    });
+    console.log(array)
+    // console.log(elm)
+    console.log(filterItems)
+    console.log(arr)
+    // console.log(unko)
+    // console.log(city)
+
+    // let elements = document.querySelectorAll('h2');
+    // for (const [i, e] of filterItems.entries()) {
+    //   e.id = `id-${i}`;
+    // }
+    
+    // tocbot.refresh()
+    // return () => tocbot.destroy()
+  }, [])
+  
+
   return (
     <Layout home>
       <Head>
@@ -32,14 +71,22 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
+        <div ref={ ref } className='blog-detail-content'>
+          <h2>これはh2タグの文章1です</h2>
+          <h2>これはh2タグの文章2です</h2>
+          <h2>これはh2タグの文章3です</h2>
+          <p>これはPタグの文章です</p>
+        </div>
       </section>
     </Layout>
   )
