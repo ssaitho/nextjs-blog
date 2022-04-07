dcd916a9212d011df7afe17ef435625579ea79c5
Shinsuke Saito <s.s.saito108@gmail.com>
2022-04-07 21:07:26 +0900

    add type script design tokens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/global.css';
+import '../styles/base.scss'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/index.tsx
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
+import { useEffect, useRef, useState } from "react";
+import Link from 'next/link'
+import Date from '../components/date'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section>
+        <p>
+          Hi, I'm Shinsuke Saito is a front-end engineer loves baseball of Tokyo.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section>
+        <h2>Blog</h2>
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <div className='bg-primary'>
+          あああああああああ
+        </div>
+        <div className='bg-danger'>
+          あああああああああ
+        </div>
+      </section>
+    </Layout>
+  )
+}
