4ee7344e2e6087d5d3ab16c4048dff7a31340277
ssaitho <ssaitho@users.noreply.github.com>
2022-04-26 10:32:38 +0000

    Update Tokens

---
+++ javascript/tokens.js
@@ -0,0 +1,220 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 26 Apr 2022 10:32:37 GMT
+ */
+
+module.exports = {
+  "primary": {
+    "value": "#0061FF",
+    "type": "color",
+    "hover": {
+      "value": "#0044B2",
+      "type": "color",
+      "filePath": "tokens/input.json",
+      "isSource": true
+    },
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#0061FF",
+      "type": "color",
+      "hover": {
+        "value": "#0044B2",
+        "type": "color",
+        "filePath": "tokens/input.json",
+        "isSource": true
+      }
+    },
+    "name": "Primary",
+    "attributes": {
+      "category": "primary"
+    },
+    "path": [
+      "primary"
+    ]
+  },
+  "danger": {
+    "value": "#ffa7a1",
+    "type": "color",
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#ffa7a1",
+      "type": "color"
+    },
+    "name": "Danger",
+    "attributes": {
+      "category": "danger"
+    },
+    "path": [
+      "danger"
+    ]
+  },
+  "secondary": {
+    "value": "#07256C",
+    "type": "color",
+    "description": "#07256C",
+    "hover": {
+      "value": "#041640",
+      "type": "color",
+      "filePath": "tokens/input.json",
+      "isSource": true
+    },
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#07256C",
+      "type": "color",
+      "description": "#07256C",
+      "hover": {
+        "value": "#041640",
+        "type": "color",
+        "filePath": "tokens/input.json",
+        "isSource": true
+      }
+    },
+    "name": "Secondary",
+    "attributes": {
+      "category": "secondary"
+    },
+    "path": [
+      "secondary"
+    ]
+  },
+  "tttttt": {
+    "value": "#434343",
+    "type": "color",
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#434343",
+      "type": "color"
+    },
+    "name": "Tttttt",
+    "attributes": {
+      "category": "tttttt"
+    },
+    "path": [
+      "tttttt"
+    ]
+  },
+  "fffffff": {
+    "value": "#ffffff",
+    "type": "color",
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#ffffff",
+      "type": "color"
+    },
+    "name": "Fffffff",
+    "attributes": {
+      "category": "fffffff"
+    },
+    "path": [
+      "fffffff"
+    ]
+  },
+  "hhhhhh": {
+    "value": "#123456",
+    "type": "color",
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#123456",
+      "type": "color"
+    },
+    "name": "Hhhhhh",
+    "attributes": {
+      "category": "hhhhhh"
+    },
+    "path": [
+      "hhhhhh"
+    ]
+  },
+  "ggggggg": {
+    "value": "#098788",
+    "type": "color",
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#098788",
+      "type": "color"
+    },
+    "name": "Ggggggg",
+    "attributes": {
+      "category": "ggggggg"
+    },
+    "path": [
+      "ggggggg"
+    ]
+  },
+  "hogehogehogehoge": {
+    "value": "#777777",
+    "type": "color",
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#777777",
+      "type": "color"
+    },
+    "name": "Hogehogehogehoge",
+    "attributes": {
+      "category": "hogehogehogehoge"
+    },
+    "path": [
+      "hogehogehogehoge"
+    ]
+  },
+  "fungafunga": {
+    "value": "#444444",
+    "type": "color",
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#444444",
+      "type": "color"
+    },
+    "name": "Fungafunga",
+    "attributes": {
+      "category": "fungafunga"
+    },
+    "path": [
+      "fungafunga"
+    ]
+  },
+  "uuuuuuuuooo": {
+    "value": "#989898",
+    "type": "color",
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#989898",
+      "type": "color"
+    },
+    "name": "Uuuuuuuuooo",
+    "attributes": {
+      "category": "uuuuuuuuooo"
+    },
+    "path": [
+      "uuuuuuuuooo"
+    ]
+  },
+  "kokokokoko": {
+    "value": "#898990",
+    "type": "color",
+    "filePath": "tokens/input.json",
+    "isSource": true,
+    "original": {
+      "value": "#898990",
+      "type": "color"
+    },
+    "name": "Kokokokoko",
+    "attributes": {
+      "category": "kokokokoko"
+    },
+    "path": [
+      "kokokokoko"
+    ]
+  }
+};
