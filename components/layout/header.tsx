a2f3e802880e0d513d2c73b0e259a28c145a1e34
s-saito <s.s.saito108@gmail.com>
2022-07-22 03:05:08 +0900

    add eslint

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,15 +4,12 @@ import component from '@ssaitho/react_ts_storybook';
 export default function Header() {
 
   return (
-    <div>
-      <component.Header/>
-    </div>
-    // <header>
-    //   <nav className="bg-primary py-4">
-    //     <div className="flex flex-col px-4 mx-auto w-full max-w-5xl lg:flex-row lg:justify-between lg:items-center">
-    //       <h1 className="text-2xl text-white">header</h1>
-    //     </div>
-    //   </nav>
-    // </header>
+    <header>
+      <nav>
+        <div>
+          <h1>header</h1>
+        </div>
+      </nav>
+    </header>
   )
 }
